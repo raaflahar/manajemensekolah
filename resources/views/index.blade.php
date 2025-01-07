e2b6de7be1bb843a9c525247c80e337a4b694945
raaflahar <raaflahar@gmail.com>
2025-01-07 20:56:55 +0700

    penambahan fitur

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,30 @@
           <a href='admin/creates' class="btn btn-primary">+ Tambah Data Kelas</a>
         </div>
 
+        <h1>Daftar Kelas</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th class="col-md-4">Kelas</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data3 as $item)
+                <tr>
+                    <td>{{$item->kelas}}</td>
+                    <td>
+                        <a href='{{ url('kelas/'.$item->kelas.'/edit')}}' class="btn btn-warning btn-sm">Edit</a>
+                        <form onsubmit="return confirm('Yakin untuk menghapus data?')" class="d-inline" action="{{ url('kelas'.$item->kelas) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" name="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
         <h1>Daftar Siswa</h1>
         <div class="pb-3">
             <form class="d-flex" action=" {{ url('admin') }}" method="get">
@@ -64,30 +88,6 @@
                     <td>
                         <a href='{{ url('guru/'.$item->guru.'/edit')}}' class="btn btn-warning btn-sm">Edit</a>
                         <form onsubmit="return confirm('Yakin untuk menghapus data?')" class="d-inline" action="{{ url('guru'.$item->guru) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" name="submit" class="btn btn-danger btn-sm">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <h1>Daftar Kelas</h1>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th class="col-md-4">Kelas</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($data3 as $item)
-                <tr>
-                    <td>{{$item->kelas}}</td>
-                    <td>
-                        <a href='{{ url('kelas/'.$item->kelas.'/edit')}}' class="btn btn-warning btn-sm">Edit</a>
-                        <form onsubmit="return confirm('Yakin untuk menghapus data?')" class="d-inline" action="{{ url('kelas'.$item->kelas) }}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit" name="submit" class="btn btn-danger btn-sm">Delete</button>
